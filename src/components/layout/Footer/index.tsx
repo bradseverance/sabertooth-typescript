e294fa223bf4f662801da1476b64827a18fe2e21
Brad Severance <bseverance@Mac.lan>
2025-07-22 09:05:01 -0400

    Add version to footer.

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,14 +1,11 @@
 import css from './Footer.module.css'
+import packageJson from '../../../../package.json'
 
 export default function Footer() {
     return (
         <footer className={css.footer}>
-        POLYWOOD&reg; Saber Tooth v 1.1 |
-        Git Data [Branch: next-js |
-        Commit Hash: 5589ddc63f24aadc93ffac4f71524295f5ae9b37 |
-        Version Date: 2026-08-01 00:00:01] |
-        DB [saber-tooth-db.polywood_scheduler] |
-        Acumatica [Polywood lcl dev]
+        POLYWOOD&reg; Saber Tooth Version 1.1 |
+        Client Version {packageJson.version}
       </footer>
     )
 }
