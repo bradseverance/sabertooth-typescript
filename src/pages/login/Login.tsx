3a3ccf604de8786c08ec5bc907bffc0e00ffe152
Brad Severance <bseverance@Mac.lan>
2025-07-23 15:17:24 -0400

    CSS and HTML markup for login form.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import css from './Login.module.css'
+import { Link } from 'react-router-dom'
 
 
 
@@ -16,15 +17,29 @@ export default function Login() {
             <h1>Log in to Your Account</h1>
             <div className={css['login-wrapper']}>
                 <form>
-                    <div className={css['login-input-wrapper']}>
+                    <div className={css['input-wrapper']}>
                         <input className={css['login-input']} type='text' placeholder='Username' />
                     </div>
-                    <div className={css['login-input-wrapper']}>
+                    <div className={css['input-wrapper']}>
                         <input className={css['login-input']} type='password' placeholder='Password' />
                     </div>
-                    
+                    <div className={css['remember-me-wrapper']}>
+                        <input type='checkbox' name='rememberMe' />
+                        <label>Remember Me</label>
+                    </div>
+                    <div className={css['campus-wrapper']}>
+                        <label>Campus</label>
+                        <select name='selectedCampus'>
+                            <option value="">Please Select Campus</option>
+                            <option>Syracuse - IN</option>
+                            <option>Roxboro - NC</option>
+                            <option>Wayfair Castlegate</option>
+                            <option>Overstock 3PL</option>
+                        </select>
+                    </div>                
                 </form>
             </div>
+            <Link to='/touch-login'>Touch Login</Link>
         </>  
     )
 }
